7bc010a1334f9994c8e87d1b03e6ac0246a32f87
Zachary Truong <zacharytruong@gmail.com>
2022-11-12 17:51:21 -0600

    working with next-config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e01570f2c56f708"; }
+/******/ 		__webpack_require__.h = function() { return "48e1f9a72f4e1d85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
